8e88a7f0165dd1d8e0989f00995244659b71f804
Navyashree Harish <navya@hyperverge.co>
2023-02-03 19:44:02 +0530

    Initial changes

---
+++ dist/index.js
@@ -0,0 +1,87 @@
+const fs = require('fs').promises;
+const path = require('path');
+const proc = require('process');
+const { authenticate } = require('@google-cloud/local-auth');
+const { google } = require('googleapis');
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(proc.cwd(), '/src/token.json');
+const CREDENTIALS_PATH = path.join(proc.cwd(), '/src/credentials.json');
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+    try {
+        const content = await fs.readFile(TOKEN_PATH);
+        const credentials = JSON.parse(content);
+        return google.auth.fromJSON(credentials);
+    }
+    catch (err) {
+        return null;
+    }
+}
+/**
+ * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+    const content = await fs.readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content);
+    const key = keys.installed || keys.web;
+    const payload = JSON.stringify({
+        type: 'authorized_user',
+        client_id: key.client_id,
+        client_secret: key.client_secret,
+        refresh_token: client.credentials.refresh_token,
+    });
+    await fs.writeFile(TOKEN_PATH, payload);
+}
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+    let client = await loadSavedCredentialsIfExist();
+    if (client) {
+        return client;
+    }
+    client = await authenticate({
+        scopes: SCOPES,
+        keyfilePath: CREDENTIALS_PATH,
+    });
+    if (client.credentials) {
+        await saveCredentials(client);
+    }
+    return client;
+}
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function getMessages(auth) {
+    const gmail = google.gmail({ version: 'v1', auth });
+    const messagesList = await gmail.users.messages.list({
+        userId: 'me',
+        labelIds: ['STARRED'],
+        maxResults: 5,
+    });
+    const messageIds = messagesList.data.messages.map((value) => value.id);
+    messageIds.forEach(async (i) => {
+        const readMessage = await gmail.users.messages.get({
+            userId: 'me',
+            id: i,
+        });
+        const obj = readMessage.data.payload.headers.find((a) => a.name === 'Subject').value;
+        console.log(obj);
+    });
+}
+authorize().then(getMessages).catch(console.error);
+//# sourceMappingURL=index.js.map
